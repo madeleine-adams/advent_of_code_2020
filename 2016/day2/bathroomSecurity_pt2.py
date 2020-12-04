f0e076ba05f1b2d6dc7a7e6878e503a19edbb4ce
Madeleine Adams <madeleineadams@WTA-MBP498.local>
2020-12-03 23:33:05 -0500

    part 2 of day 2, 2016

--- 2016/day2/bathroomSecurity_pt2.py
+++ 2016/day2/bathroomSecurity_pt2.py
@@ -5,18 +5,40 @@ position = (0, 2)
 
 
 def digit_for_position(coordinate):
-    digit = coordinate[0] + 1
-    digit += (2-coordinate[1]) * 3
+    if coordinate[1] == 4:
+        return "1"
+    elif coordinate[1] == 3:
+        return str(coordinate[0] + 1)
+    elif coordinate[1] == 2:
+        return str(coordinate[0] + 5)
+    elif coordinate[1] == 1:
+        if coordinate[0] == 1:
+            return "A"
+        elif coordinate[0] == 2:
+            return "B"
+        else:
+            return "C"
+    else:
+        return "D"
     return digit
 
+
 def is_edge(direction, coordinate):
+    x = coordinate[0]
+    y = coordinate[1]
     if direction == "R":
-
+        if y == (x - 2) or y == (6 - x):
+            return True
     elif direction == "L":
-
+        if y == (x + 2) or y == (2 - x):
+            return True
     elif direction == "U":
-
+        if y == (x + 2) or y == (6 - x):
+            return True
     else:
+        if y == (x - 2) or y == (2 - x):
+            return True
+    return False
 
 
 for line in lines_read:
